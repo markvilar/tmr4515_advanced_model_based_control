d7a7d6646c61c0f1faf671e010b982bb9acf4e7d
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2019-10-31 13:00:17 +0100

    Updated VanLoan.m.

--- project/src/VanLoan.m
+++ project/src/VanLoan.m
@@ -1,17 +1,28 @@
-function [F, Q] = VanLoan(A, G, D, Ts)
-% Discretizes a stochastic LTI system.
-% arg A: nxn array of floats, continous time transition matrix
-% arg G: nxk array of floats, continous time process noise gain
-% arg D: kxk array of floats, continous time process noise covariance
+function [Ad, Bd, Qd] = VanLoan(A, B, G, D, Ts)
+% Discretization of stochastic LTI systems using Van Loans method.
+% arg A: nxn array of floats, continuous time transition matrix
+% arg B: nxj array of floats, continuous time input matrix
+% arg G: nxk array of floats, continuous time process noise gain matrix
+% arg D: kxk array of floats, continuous time process noise covariances
 % arg Ts: float, sampling time
-% return F: nxn array of floats
+% return Ad: nxn array of floats
+% return Bd: nxj array of floats
 % return Q: nxn array of floats
 n = size(A, 1);
-M = expm([-A G*D*G'; zeros(n, n) A']*Ts);
-V1 = M(n+1:2*n, n+1:2*n);
-V2 = M(1:n, n+1:2*n);
+j = size(B, 2);
 
-F = V1';
-Q = F*V2;
+% Calculate Van Loan matrices
+M = expm([-A G*D*G'; zeros(n, n) A']*Ts);
+N = expm([A B; zeros(j, n) zeros(j, j)]*Ts);
+
+% Extract sub matrices
+M11 = M(1:n, 1:n);
+M12 = M(1:n, n+1:n+n);
+N12 = N(1:n, n+1:n+j);
+
+% Compute discretized system matrices
+Ad = M11;
+Bd = N12;
+Qd = M12*M11';
 end
 
