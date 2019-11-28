c5107131dc7ac5077df6aed5618560e54db9a62c
Martin Kvisvik Larsen <martin.kvisvik.larsen@hotmail.com>
2019-11-28 11:09:13 +0100

    Added run variable in main.m.

--- project/src/main.m
+++ project/src/main.m
@@ -1,7 +1,13 @@
-%% Initialization of model
-clear all
-clc
+%% General
+clear all; close all; clc;
+runSimulation = true;
 
+%% Initialization of models
 run('start_up.m');
 run('initTask1.m');
 run('initTask2.m');
+
+%% Run
+if runSimulation
+    run('simulation.slx');
+end
